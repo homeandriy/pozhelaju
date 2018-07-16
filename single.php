53b4e265881689104bc3549e6b00542e088bfd87
andrii.beznosko <andrii.beznosko@volia.com>
2018-07-16 14:06:32 +0300

    init add single.php

--- single.php
+++ single.php
@@ -11,27 +11,31 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main container">
+            <div class="row">
+                <div class="col-sm-3"><h2>menu</h2></div>
+                <div class="col-sm-6">
+                    <?php
+                    while ( have_posts() ) :
+                        the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+                        get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+                        the_post_navigation();
 
-			the_post_navigation();
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+                    endwhile; // End of the loop.
+                    ?>
+                </div>
+                <div class="col-sm-3"><?php  get_sidebar(); ?></div>
+            </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
