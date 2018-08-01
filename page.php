6f0929811723eecaf5d566166ae0976220172e49
andrii.beznosko <andrii.beznosko@volia.com>
2018-08-01 17:47:41 +0300

    5. Додана розмітка page.php

--- page.php
+++ page.php
@@ -17,21 +17,39 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+            <main id="main" class="site-main container">
+                <div class="row">
+                    <div class="col-lg-3  d-none d-lg-block"><?php  get_template_part('template-parts/menu', 'global') ?></div>
+                    <div class="col-lg-6 col-sm-12 col-xs-12 main-content">
+                        <section class="" itemscope itemtype="http://schema.org/Article" >
+                            <div class="wrapper-post single-list-category bootom-border shadow" >
+                                <?php if(function_exists('bcn_display'))
+                                {
+                                    bcn_display();
+                                }?>
+                            </div>
+                        </section>
+                        <section <?php post_class()?> itemscope itemtype="http://schema.org/Article" >
+                            <div class="wrapper-post" >
+                                <?php
+                                while ( have_posts() ) :
+                                    the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+                                    get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
+                                    // If comments are open or we have at least one comment, load up the comment template.
+                                    if ( comments_open() || get_comments_number() ) :
+                                        comments_template();
+                                    endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+                                endwhile; // End of the loop.
+                                ?>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="col-lg-3  d-none d-lg-block"><?php  get_sidebar(); ?></div>
+                </div>
+            </main>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
