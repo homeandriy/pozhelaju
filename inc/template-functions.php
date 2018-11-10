1d7bf4b4388192c8bbcc2a0ddd3d24898075914e
homeandriy <homeandriy@gmail.com>
2018-11-10 23:53:57 +0200

    Перероблено календар повнісю+ додано ефекти

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,71 @@ function pozhelaju_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'pozhelaju_pingback_header' );
+
+
+add_shortcode( 'insert_holidays', 'insert_holidays_function' );
+
+
+function insert_holidays_function() {
+	global $wpdb;
+	$holiday_table = $wpdb->prefix . 'termmeta';
+
+	$get_all_holidays_by_range = $wpdb->get_results("SELECT * FROM $holiday_table WHERE meta_key = '__day_of_holiday' AND meta_value BETWEEN (NOW() - INTERVAL 1 DAY) AND  (NOW() + INTERVAL 7 DAY)");
+
+	$html = '<ul class="list-holidays">';
+	if( $get_all_holidays_by_range and count($get_all_holidays_by_range ) >= 1) {
+		$current_date = $get_all_holidays_by_range[0]->meta_value;
+		$f_date = new DateTime( $current_date );
+
+		$html .='<li><i class="fa fa-chevron-right"></i> <strong>'. $f_date->format('j').' '. returnRusName($f_date->format('m')) .'</strong> </li>';
+
+		foreach ($get_all_holidays_by_range as $holiday ) {
+
+			// $res = var_dump( $current_date  == $holiday->meta_value );
+			// echo $current_date ."=== ".$holiday->meta_value." - ".$res ."<br>";
+			if( $current_date  == $holiday->meta_value ) {
+				$f_date = new DateTime( $holiday->meta_value );
+				$category = get_category( $holiday->term_id );
+
+				$html .='<li><a href="'.get_category_link( $holiday->term_id ).'" class="link-holiday">'.$category->name.'</a></li>';
+			}
+			else {
+				$current_date = $holiday->meta_value;
+				$f_date = new DateTime( $holiday->meta_value );
+				$category = get_category( $holiday->term_id );
+
+				$html .='<li><i class="fa fa-chevron-right"></i> <strong>'. $f_date->format('j').' '. returnRusName($f_date->format('m')) .' </strong> </li>';
+				$html .='<li><a href="'.get_category_link( $holiday->term_id ).'" class="link-holiday">'.$category->name.'</a></li>';
+			}
+		}
+	}
+	$html .= '</ul>';
+	return $html;
+	// $get_all_holidays_by_range = $wpdb->get_results("SELECT * FROM $holiday_table WHERE meta_key = '__day_of_holiday'");
+
+	// foreach ($get_all_holidays_by_range as $ddd) {
+	// 	$formatdate = new DateTime( $ddd->meta_value );
+	// 	$date = $formatdate->format('Y-m-d');
+
+	// 	update_term_meta( $ddd->term_id, '__day_of_holiday', $date );
+	// 	echo $date."<br>";
+	// }
+}
+
+function returnRusName ( $id ){
+	$rusMounth = array( 
+		"01" => "января", 
+		"02" => "февраля", 
+		"03" => "марта", 
+		"04" => "апреля", 
+		"05" => "мая", 
+		"06" => "июня", 
+		"07" => "июля", 
+		"08" => "августа", 
+		"09" => "сентября", 
+		"10" => "октября", 
+		"11" => "ноября", 
+		"12" => "декабря"
+	);
+	return $rusMounth[$id];
+}
